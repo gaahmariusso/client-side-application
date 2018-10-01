d0038a5ff013c0e2bf400458707ca042b6053c04
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2018-10-01 02:25:06 -0300

    Implementação do direcionamento de página (repositories.html) e tratamento para 403/404 com modal.

--- JScript.js
+++ JScript.js
@@ -4,6 +4,30 @@ $( "#form_usu" ).submit(function() {
 	$.ajax({
 		method: "GET",
 		url: "https://api.github.com/users/"+user,
+		statusCode: {
+			404: function(request, status, error) {
+				console.log("404 - Not Found");
+	            console.log(error);
+				// var modal = document.getElementById('myModal');
+				// var btn = document.getElementById("btn_search");
+				// var span = document.getElementsByClassName("close")[0];
+				// btn.onclick = function() {
+    // 			modal.style.display = "block";
+				// }
+				// span.onclick = function() {
+    // 			modal.style.display = "none";
+				// }
+				// window.onclick = function(event) {
+				//     if (event.target == modal) {
+				//         modal.style.display = "none";
+				//     }
+				// }
+			},
+			403: function(request, status, error) {
+				console.log("403 - Not Found");
+	            console.log(error);
+	        }
+		}
 	}).done(function(result) {
 
 		$("#avatar_url").attr("src",result.avatar_url).attr("height",150);
@@ -17,11 +41,15 @@ $( "#form_usu" ).submit(function() {
 			url: "https://api.github.com/users/"+user+"/repos",
 		}).done(function(result) {
 
+			result.sort(function(a,b){
+				return b.stargazers_count - a.stargazers_count;
+			});
+			console.log(result);
 			$('#repositories').empty();
 			for(i in result) {
 				var repositories = result[i];
 				console.log(result[i]);
-				$('#repositories').append("<ul><li><span id='repos_url'> <a target='_blank' href=''>"+result[i].name+"</a></span></li>");
+				$('#repositories').append("<ul><li><span id='repos_url'> <a target='_blank' href='repositories.html'>"+result[i].name+"</a></span></li>");
 			}
 		});
 	});
